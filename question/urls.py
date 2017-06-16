bb745a2c2181205f5e92fbccfa53dae69eea51f4
Kishy-nivas <kishynivas10@gmail.com>
2017-06-16 23:40:20 +0530

    changed homepage

--- question/urls.py
+++ question/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url,include
 from .import views
 
 urlpatterns =[
-	url(r'^$',views.index,name='index'),
+	url(r'^home/$',views.index,name='index'),
 	url(r'^signup/$',views.signup,name='signup'),
 	url(r'^profile/(?P<pk>\d+)/$',views.profiledetail.as_view(),name = 'profile-detail'),
 	url(r'^ask/$',views.ask, name='ask'),
-	url(r'^question/$',views.question_list.as_view(),name='question-list'),
+	url(r'^$',views.question_list.as_view(),name='question-list'),
 	url(r'^questiondetail/(?P<pk>\d+)/$',views.question_detail ,name ='question-detail'),
 	url(r'answer/$',views.answer ,name ='answer'),	 
 ]
